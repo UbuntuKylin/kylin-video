29f48bf3b1ae69b70c05066d095459c8c527c7c6
balloonflower <kobe24_lixiang@126.com>
2018-06-11 17:46:54 +0800

    Solved the problem of the failure of the loaded translation file

--- src/smplayer/translator.cpp
+++ src/smplayer/translator.cpp
@@ -32,7 +32,7 @@ Translator::~Translator() {
 }
 
 bool Translator::loadCatalog(QTranslator & t, QString name, QString locale, QString dir) {
-	QString s = name + "_" + locale; //.toLower();
+        QString s = name + "_" + locale + ".pm"; //.toLower();
 	bool r = t.load(s, dir);
 	if (r) 
 		qDebug("Translator::loadCatalog: successfully loaded %s from %s", s.toUtf8().data(), dir.toUtf8().data());

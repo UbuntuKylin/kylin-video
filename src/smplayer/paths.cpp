34647b78034830d14a158aa4008f0273a2991f35
balloonflower <kobe24_lixiang@126.com>
2018-06-12 17:03:19 +0800

    Rewrite Paths::qtTranslationPath() for merging debian and snapcraft

--- src/smplayer/paths.cpp
+++ src/smplayer/paths.cpp
@@ -54,7 +54,10 @@ QString Paths::shortcutsPath() {
 }
 
 QString Paths::qtTranslationPath() {
-    return QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+    if (app_path.startsWith("/snap/kylin-video/"))// /snap/kylin-video/x1/usr/bin
+        return "/snap/kylin-video/current" + QLibraryInfo::location(QLibraryInfo::TranslationsPath);// /snap/kylin-video/current/usr/share/qt5/translations
+    else
+        return QLibraryInfo::location(QLibraryInfo::TranslationsPath);// /usr/share/qt5/translations
 }
 
 void Paths::setConfigPath(QString path) {

30b82d77cdc4f58d31657ac585afa856448625de
handsome-feng <jianfengli@ubuntukylin.com>
2020-10-26 19:49:23 +0800

    Update changelog and copyright

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
     a.setQuitOnLastWindowClosed(false);
     a.setOrganizationName("kylin");
     a.setApplicationName("kylin-video");
-    a.setApplicationVersion("2.1.0");
+    a.setApplicationVersion("2.1.1");
 
 #if QT_VERSION >= 0x040400
     // Enable icons in menus
